e4b0e222b5ea7bf812ae04f619a4b39f19ff6b57
Cameron <cameron.wills89@outlook.com>
2021-01-10 14:46:57 -0500

    implemented update and delete functionality to the cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -38,7 +38,7 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   useEffect(() => {
     //check if there's data in the global state products array
@@ -55,9 +55,26 @@ function Detail() {
   }, [products, data, dispatch, id]); //This is known as the dependency array.
 
   const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 },
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
     });
   };
 
@@ -74,7 +91,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
